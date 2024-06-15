54927179e014cd2f35aafd03ac0b0f34e144bd23
Anjuptpn <appcensopalomar@gmail.com>
2024-06-15 16:58:05 +0100

    Corregidos errores en la vista del Perfil de Usuario

---
+++ src/app/pages/user/edit-pigeon/edit-pigeon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-pigeon',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-pigeon.component.html',
+  styleUrl: './edit-pigeon.component.sass'
+})
+export class EditPigeonComponent {
+
+}
---
+++ src/app/pages/user/view-pigeon/view-pigeon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-pigeon',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-pigeon.component.html',
+  styleUrl: './view-pigeon.component.sass'
+})
+export class ViewPigeonComponent {
+
+}
